ad51a562f34b26fd2dc5bba3994ca5d3c2eb96be
DjRedstone <djredstone64@gmail.com>
2022-08-03 18:12:51 +0200

    Quelques modification du design (police et fond adaptés), ajout d'une étape finale, finition de l'annimation d'ajout de points

--- private/client.js
+++ private/client.js
@@ -15,7 +15,7 @@ $.getJSON("data.json", (data) => {
     infos.jury_nb = DATA.jury_points.length;
     infos.countries_nb = DATA.countries.length;
 
-    let moving= -400;
+    let moving= -500;
     for (i = 0; i < data.countries.length; i++) {
         const country = data.countries[i];
         const countryID = getID(country.name);
@@ -42,25 +42,32 @@ function getIndex(id, table) {
 }
 
 function addPoints(id, points) {
-    const countryPointsIndex = getIndex(id, POINTS);
+    let countryPointsIndex = getIndex(id, POINTS);
     POINTS[countryPointsIndex].points += points;
     const addedPointsElement = $(`#${id}-added-points`);
     addedPointsElement.text(points);
     addedPointsElement.css("opacity", 1);
+    addedPointsElement.css("right", "150px");
     setTimeout(() => {
-        addedPointsElement.css("right", "-100px");
+        sort();
         setTimeout(() => {
-            $(`#${id}-points`).text(POINTS[countryPointsIndex].points);
-            setTimeout(sort, 500);
-        }, 500);
-    }, 2000);
+            addedPointsElement.css("right", "-100px").delay(1000);
+            setTimeout(() => {
+                countryPointsIndex = getIndex(id, POINTS);
+                $(`#${id}-points`).text(POINTS[countryPointsIndex].points);
+                setTimeout(() => {
+                    $(`#${id}`).css("background", "#D74703");
+                }, 1000);
+            }, 250);
+        }, 2000);
+    }, 3000);
 }
 
 function sort() {
     POINTS.sort((a, b) => {
         return b.points - a.points
     });
-    let moving= -400;
+    let moving= -500;
     for (i = (POINTS.length - 1); i >= 0; i--) {
         const country = POINTS[i];
         $(`#${country.id}`).css("bottom", `${moving}px`);
@@ -73,7 +80,24 @@ function next() {
         console.log(`Stade jury (${stade})`);
     } else {
         const newStade = stade - infos.jury_nb;
-        if (newStade >= infos.countries_nb) return;
+        if (newStade == infos.countries_nb) {
+            let moving = -500;
+            for (i = 0; i < POINTS.length; i++) {
+                const place = (i + 1);
+                const countryID = POINTS[i].id;
+                const countryEl = $(`#${countryID}`);
+                $(`<div class="place" id="${countryID}-place"><div class="place-number">0${place}</div></div>`).insertBefore(countryEl);
+                $(`#${countryID}-place`).css("bottom", countryEl.css("bottom"));
+                $(`#${countryID}-place`).css("transform", "translateX(-310px)");
+                countryEl.css("background", "linear-gradient(4deg, #1b1428 0%, #1b1428 40%, #6c2732 90%)");
+                countryEl.css("transform", "translateX(100px)");
+                countryEl.css("width", "675px");
+                moving += 200;
+            }
+            $(`<div id="title">Results of the Eurovision Smoot Contest 2022</div>`).insertBefore("#countries");
+            stade ++;
+            return;
+        } else if (newStade > infos.countries_nb) return;
         const countryID = getID(DATA.countries[newStade].name);
         const pointsToAddIndex = getIndex(countryID, publicPoints);
         const pointsToAdd = publicPoints[pointsToAddIndex].points;
