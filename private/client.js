ea86877459e8fc4aeb3372cdd676235b62370067
DjRedstone <djredstone64@gmail.com>
2022-11-28 21:17:46 +0100

    Passage des données par un google sheets

--- private/client.js
+++ private/client.js
@@ -1,11 +1,13 @@
 
-$.getJSON("data.json", (data) => {
+const socket = io();
+
+socket.on("data", (data) => {
     const stade = new Stade(data, {
         VOTE_MOVING_START: -800,
         VOTE_MOVING_IN: 160,
         FINAL_MOVING_START: -850,
         FINAL_MOVING_IN: 150,
-        MIN_POINTS_PLAYSOUND: 6
+        MIN_POINTS_PLAYSOUND: 100
     });
 
     document.body.onkeyup = (e) => {
--- server.js
+++ server.js
@@ -16,6 +16,15 @@ require("dotenv").config();
 
 const secretToken = process.env.SECRET_TOKEN;
 
+const { google } = require("googleapis");
+
+const client = new google.auth.JWT(
+    process.env.CLIENT_EMAIL,
+    null,
+    process.env.PRIVATE_KEY,
+    ["https://www.googleapis.com/auth/spreadsheets"]
+);
+
 app.use("/jquery", express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use(express.static("public"));
 app.use(express.static("private"))
@@ -36,6 +45,50 @@ http.listen(port, () => {
     console.log(`App server is running on port ${port}`);
 });
 
-io.on("connection", (socket) => {
-    
+client.authorize((err, tokens) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Connected to Google Sheets API !");
+    }
 });
+
+io.on("connection", (socket) => {
+    gsrun(client, "A:Z").then((data) => {
+        newData = {};
+        let i = 0;
+
+        newData.countries = [];
+        while (i < data[0].slice(2).length) {
+            newData.countries.push({name: data[0].slice(2)[i], flag: data[1].slice(2)[i]});
+            i++;
+        }
+
+        i = 0;
+        newData.jury_points = [];
+        while (i < data[2].slice(2).length) {
+            newData.jury_points.push({name: data[2].slice(2)[i], points: JSON.parse(data[3].slice(2)[i])});
+            i++;
+        }
+
+        i = 0;
+        newData.public_points = [];
+        while (i < data[4].slice(2).length) {
+            newData.public_points.push({id: data[4].slice(2)[i], points: parseInt(data[5].slice(2)[i])});
+            i++;
+        }
+
+        socket.emit("data", newData);
+    });
+});
+
+async function gsrun(cl, ran) {
+    const gsapi = google.sheets({version: "v4", auth: cl});
+    const opt = {
+      spreadsheetId: "1eORVqZygzxbzqiZt5J47D2PY85UA_Nh5obWc03B1M3c",
+      range: ran
+    };
+    data = await gsapi.spreadsheets.values.get(opt);
+    return data.data.values;
+}
